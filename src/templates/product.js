28fe33823deacffe45ea449c32483e5465d25f54
Luke Bennett <hello@lukebennett.com.au>
2020-08-29 12:51:18 +1000

    Save progress

--- src/templates/product.js
+++ src/templates/product.js
@@ -2,12 +2,7 @@ import React, { useEffect, useMemo, useState } from 'react';
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 
-import {
-  useAddItemToCart,
-  useGraphQL,
-  useLazyLoad,
-  useStoreContext,
-} from '../hooks';
+import { useAddItemToCart, useGraphQL, useLazyLoad } from '../hooks';
 import {
   prepareVariantsWithOptions,
   prepareVariantsImages,
@@ -18,35 +13,81 @@ import { Layout, SEO, OptionPicker, Thumbnail } from '../components';
 export default function ProductPageTemplate({
   data: { shopifyProduct: product },
 }) {
-  // Get available colors
-  const colors =
-    product.options.find((option) => option.name.toLowerCase() === 'color')
+  // Lazy load images using custom hook
+  const { ref, imgRef, isImgLoaded, handleImgLoaded, Spinner } = useLazyLoad();
+
+  // Get all possible extras
+  const extras =
+    product.options.find((option) => option.name.toLowerCase() === 'extras')
       ?.values || [];
 
-  // Get available sizes
+  // Get all possible sizes
   const sizes =
     product.options.find((option) => option.name.toLowerCase() === 'size')
       ?.values || [];
 
-  // Format the data we get back from GraphQL for variants to be a little
-  // easier to work with.
+  // Format the data we get back from GraphQL for variants to be a little easier to work with
   // See comment in `prepare-variants-with-options.js`
   const variants = useMemo(() => prepareVariantsWithOptions(product.variants), [
     product.variants,
   ]);
 
-  // Format the data we get back from GraphQL for images to be a little easier
-  // to work with.
+  // Keep variants in state, and set the default variant to be the first available item
+  const [variant, setVariant] = useState(
+    variants.find((v) => v.availableForSale === true) || variants[0]
+  );
+
+  // Format the data we get back from GraphQL for images to be a little easier to work with
   // See comment in `prepare-variants-images.js`
-  const images = useMemo(() => prepareVariantsImages(variants, 'color'), [
+  const images = useMemo(() => prepareVariantsImages(variants, 'extras'), [
     variants,
   ]);
 
-  // Keep variants in state, and set the default variant to be the first item
-  const [variant, setVariant] = useState(variants[0]);
+  // Keep different extras options in state
+  const [extra, setExtra] = useState(variant.extra);
+
+  // Keep different sizes in state
+  const [size, setSize] = useState(variant.size);
+
+  const [addToCartDisabled, setAddToCartDisabled] = useState(false);
+
+  // Manage add to cart alerts in state
+  const [isAlertShown, setIsAlertShown] = useState(false);
+
+  // Use a custom hook for adding items to cart
+  const addItemToCart = useAddItemToCart();
+
+  // Whenever we add an item to the cart, also create an alert to notify the customer of this
+  // Note: we are hard coding the number of items to be added to cart as 1, we can add another useState instance to address this in the future if we need to
+  function handleAddToCart() {
+    addItemToCart(variant.shopifyId, 1);
+    setIsAlertShown(true);
+  }
+
+  // This handles adding the correct variant to the cart
+  useEffect(() => {
+    // Try to find the variant with selected size and extras
+    const newVariant = variants.find((v) => {
+      return v.size === size && v.extras === extra;
+    });
+
+    if (typeof newVariant === 'object') {
+      setVariant(newVariant);
+      setAddToCartDisabled(false);
+      if (!newVariant.availableForSale) {
+        // If variant exists but is not available for sale
+        setAddToCartDisabled(true);
+      }
+    } else {
+      // If variant doesn't exist
+      setAddToCartDisabled(true);
+    }
+  }, [size, extra, variants, variant.shopifyId]);
 
   // If product doesn't have an image, we can use a placeholder
   const { placeholderImage } = useGraphQL();
+
+  // Keep source for primary image in state
   const [imgSrc, setImgSrc] = useState(placeholderImage.publicURL);
 
   // Update the primary image whenever the variant changes
@@ -55,55 +96,15 @@ export default function ProductPageTemplate({
       setImgSrc(
         resizeShopifyImage({ url: variant.image.originalSrc, width: 800 })
       );
+      imgRef.current.src = imgSrc;
     }
-  }, [variant]);
-
-  // Keep different color options in state
-  const [color, setColor] = useState(variant.color);
-
-  // Keep different sizes in state
-  const [size, setSize] = useState(variant.size);
-
-  // Keep product quantity is state
-  const [quantity] = useState(1);
-
-  // Manage add to cart alerts in state
-  const { setAddedToCart, setCartContent } = useStoreContext();
-
-  // Use a custom hook for adding items to cart
-  const addItemToCart = useAddItemToCart(false);
-
-  // Whenever we add an item to the cart, also create an alert to notify the customer of this
-  // Note: we are hard coding the number of items to be added to cart as 1, we can add another useState instance to address this in the future if we need to
-  function handleAddToCart() {
-    if (quantity >= 1) {
-      addItemToCart(variant.shopifyId, quantity);
-      setCartContent({ title: product.title, image: imgSrc, quantity: 1 });
-      setAddedToCart(true);
-    }
-  }
-
-  // This handles adding the correct variant to the cart
-  useEffect(() => {
-    const newVariant = variants.find((v) => {
-      return v.size === size && v.color === color;
-    });
-
-    if (variant.shopifyId !== newVariant.shopifyId) {
-      setVariant(newVariant);
-    }
-  }, [size, color, variants, variant.shopifyId]);
-
-  // Lazy load images using custom hook
-  const { ref, imgRef, isImgLoaded, handleImgLoaded, Spinner } = useLazyLoad();
+  }, [variant, imgSrc, imgRef]);
 
   return (
     <Layout hasSidebar={false}>
       <SEO
         title={product.title}
-        image={
-          variant.image ? variant.image.originalSrc : placeholderImage.publicURL
-        }
+        image={variant.image && variant.image.originalSrc}
       />
       <article className="relative">
         <div className="rounded-lg md:grid md:grid-cols-2 md:gap-8">
@@ -130,9 +131,11 @@ export default function ProductPageTemplate({
               <div className="grid grid-cols-3 gap-6">
                 {images.map((img) => (
                   <Thumbnail
-                    key={img.color}
+                    key={img.extras}
                     src={img.src}
-                    handleClick={() => setColor(img.color)}
+                    onClick={() => {
+                      setExtra(img.extras);
+                    }}
                   />
                 ))}
               </div>
@@ -144,17 +147,15 @@ export default function ProductPageTemplate({
             </h1>
             <div className="font-bold">${variant.price}</div>
             {product.availableForSale &&
-              (colors.length > 1 || sizes.length > 1) && (
+              (extras.length > 1 || sizes.length > 1) && (
                 <div className="grid gap-4 mt-6 sm:grid-cols-2">
                   <OptionPicker
-                    key="Color"
-                    name="Colour"
-                    options={colors}
-                    selected={color}
-                    onChange={(event) => setColor(event.target.value)}
+                    name="Extras"
+                    options={extras}
+                    selected={extra}
+                    onChange={(event) => setExtra(event.target.value)}
                   />
                   <OptionPicker
-                    key="Size"
                     name="Size"
                     options={sizes}
                     selected={size}
@@ -166,17 +167,11 @@ export default function ProductPageTemplate({
               <button
                 onClick={handleAddToCart}
                 type="button"
-                disabled={quantity < 1 || quantity === ''}
                 className="inline-flex items-center justify-center w-full px-6 py-3 font-mono text-xl font-bold leading-6 text-pink-700 lowercase transition duration-150 ease-in-out bg-pink-100 border border-transparent rounded-full shadow-sm hover:bg-pink-50 focus:outline-none focus:border-pink-300 focus:shadow-outline-pink active:bg-pink-200 hover:shadow-lg"
               >
                 Add to Cart
               </button>
             </span>
-            {quantity === '' && (
-              <small className="absolute bottom-0 transform translate-y-6">
-                Please provide a quantity
-              </small>
-            )}
             <div
               dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
               className="grid row-gap-4 mt-6 text-base leading-6 text-gray-700"
