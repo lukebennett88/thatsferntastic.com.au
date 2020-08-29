87b145b5efc1e40214b991e0b7a2de4af0ead526
Luke Bennett <hello@lukebennett.com.au>
2020-08-29 12:50:47 +1000

    Formatting

--- src/components/thumbnail/thumbnail.js
+++ src/components/thumbnail/thumbnail.js
@@ -1,16 +1,35 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Image from 'gatsby-image';
 
-export const Thumbnail = ({ src, onClick }) => {
+import { useLazyLoad, useGraphQL } from '../../hooks';
+import { resizeShopifyImage } from '../../utils';
+
+export function Thumbnail({ src, onClick }) {
+  const { placeholderImage } = useGraphQL();
+  const { ref, imgRef, isImgLoaded, handleImgLoaded, Spinner } = useLazyLoad();
+  const imgSrc = src?.originalSrc
+    ? resizeShopifyImage({ url: src.originalSrc, width: 300 })
+    : placeholderImage.publicURL;
   return (
-    <button onClick={onClick} type="button">
-      <Image fluid={src.localFile.childImageSharp.fluid} />
+    <button
+      ref={ref}
+      type="button"
+      onClick={onClick}
+      className="relative h-0 overflow-hidden bg-white cursor-pointer aspect-ratio-square"
+    >
+      <img
+        ref={imgRef}
+        onLoad={handleImgLoaded}
+        data-src={imgSrc}
+        alt=""
+        className="absolute inset-0 flex items-center justify-center object-contain w-full h-full bg-white"
+      />
+      {!isImgLoaded && <Spinner />}
     </button>
   );
-};
+}
 
 Thumbnail.propTypes = {
-  onClick: PropTypes.func,
-  src: PropTypes.object,
+  onClick: PropTypes.func.isRequired,
+  src: PropTypes.object.isRequired,
 };
